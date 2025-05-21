1dc934475cdd509e116a38ccd8d32c558a24c550
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-05-21 08:56:55 +0200

    feat: return empty merkle path result for not mined transaction.

--- pkg/services/internal/arc/arc_get_merkle_path.go
+++ pkg/services/internal/arc/arc_get_merkle_path.go
@@ -24,39 +24,40 @@ func (s *Service) MerklePath(ctx context.Context, txID string) (*wdk.MerklePathR
 		return nil, fmt.Errorf("got response for tx %s while querying for %s", txInfo.TxID, txID)
 	}
 
-	if is.NotBlankString(txInfo.MerklePath) {
-		merklePath, err := transaction.NewMerklePathFromHex(txInfo.MerklePath)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse merkle path %s: %w", txInfo.MerklePath, err)
-		}
-		blockHeight, err := to.UInt32(txInfo.BlockHeight)
-		if err != nil {
-			return nil, fmt.Errorf("invalid block height %d in Merkle Path %s for transaction %s: %w", txInfo.BlockHeight, txInfo.MerklePath, txInfo.TxID, err)
-		}
-
-		if merklePath.BlockHeight != blockHeight {
-			return nil, fmt.Errorf("merkle path %s block height %d does not match tx block height %d", txInfo.MerklePath, merklePath.BlockHeight, txInfo.BlockHeight)
-		}
-
-		hex, err := merklePath.ComputeRootHex(&txInfo.TxID)
-		if err != nil {
-			return nil, fmt.Errorf("failed to compute block hash from merkle path %s root for tx %s: %w", txInfo.MerklePath, txInfo.TxID, err)
-		}
-
-		if hex != txInfo.BlockHash {
-			return nil, fmt.Errorf("computed block hash %s from merkle path %s does not match tx block hash %s", hex, txInfo.MerklePath, txInfo.BlockHash)
-		}
-
+	if is.BlankString(txInfo.MerklePath) {
 		return &wdk.MerklePathResult{
-			Name:       ServiceName,
-			MerklePath: merklePath,
-			Header: &wdk.BlockHeader{
-				Height: blockHeight,
-				Hash:   hex,
-			},
+			Name: ServiceName,
 		}, nil
 	}
 
-	return nil, fmt.Errorf("tx %s has no merkle path", txID)
+	merklePath, err := transaction.NewMerklePathFromHex(txInfo.MerklePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse merkle path %s: %w", txInfo.MerklePath, err)
+	}
+	blockHeight, err := to.UInt32(txInfo.BlockHeight)
+	if err != nil {
+		return nil, fmt.Errorf("invalid block height %d in merkle path %s for transaction %s: %w", txInfo.BlockHeight, txInfo.MerklePath, txInfo.TxID, err)
+	}
 
+	if merklePath.BlockHeight != blockHeight {
+		return nil, fmt.Errorf("merkle path %s block height %d does not match tx block height %d", txInfo.MerklePath, merklePath.BlockHeight, txInfo.BlockHeight)
+	}
+
+	hex, err := merklePath.ComputeRootHex(&txInfo.TxID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to compute block hash from merkle path %s root for tx %s: %w", txInfo.MerklePath, txInfo.TxID, err)
+	}
+
+	if hex != txInfo.BlockHash {
+		return nil, fmt.Errorf("computed block hash %s from merkle path %s does not match tx block hash %s", hex, txInfo.MerklePath, txInfo.BlockHash)
+	}
+
+	return &wdk.MerklePathResult{
+		Name:       ServiceName,
+		MerklePath: merklePath,
+		Header: &wdk.BlockHeader{
+			Height: blockHeight,
+			Hash:   hex,
+		},
+	}, nil
 }
