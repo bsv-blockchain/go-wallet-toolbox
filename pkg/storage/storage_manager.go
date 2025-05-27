655f8f672de679488229c78516a43b4979bb36fb
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-05-27 14:21:37 +0200

    refactor: move managed storage to internal package

--- pkg/storage/storage_manager.go
+++ pkg/storage/storage_manager.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/4chain-ag/go-wallet-toolbox/pkg/storage/internal/managed"
 	"github.com/4chain-ag/go-wallet-toolbox/pkg/wdk"
 	"github.com/go-softwarelab/common/pkg/is"
 	"github.com/go-softwarelab/common/pkg/to"
@@ -11,51 +12,12 @@ import (
 
 var _ wdk.WalletStorage = (*WalletStorageManager)(nil)
 
-type managedStorage struct {
-	wdk.WalletStorageProvider
-	settings *wdk.TableSettings
-	user     *wdk.TableUser
-}
-
-func newMangedStorage(storage wdk.WalletStorageProvider) *managedStorage {
-	return &managedStorage{
-		WalletStorageProvider: storage,
-	}
-}
-
-func (s *managedStorage) isAvailable() bool {
-	return s.settings != nil && s.user != nil
-}
-
-func (s *managedStorage) makeAvailable(ctx context.Context, identityKey string) (*wdk.TableSettings, error) {
-	if s.isAvailable() {
-		return s.settings, nil
-	}
-	settings, err := s.MakeAvailable(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to make available storage: %w", err)
-	}
-
-	userResponse, err := s.FindOrInsertUser(ctx, identityKey)
-	if err != nil {
-		return nil, fmt.Errorf("failed to find or insert user to storage %s: %w", settings.StorageName, err)
-	}
-	if userResponse.User.IdentityKey != identityKey {
-		return nil, fmt.Errorf("storage %s returned user with different identity key (%s)", settings.StorageName, userResponse.User.IdentityKey)
-	}
-
-	s.settings = settings
-	s.user = &userResponse.User
-
-	return settings, nil
-}
-
 // WalletStorageManager provides methods for managing wallet active storage and backups.
 // Also delivers authentication checking storage access to the wallet.
 type WalletStorageManager struct {
 	isAvailable   bool
 	identityKey   string
-	activeStorage *managedStorage
+	activeStorage *managed.Storage
 }
 
 // NewWalletStorageManager initializes a WalletStorageManager with an identity key and an active storage provider.
@@ -74,7 +36,7 @@ func NewWalletStorageManager(identityKey string, active wdk.WalletStorageProvide
 	}
 
 	return &WalletStorageManager{
-		activeStorage: newMangedStorage(active),
+		activeStorage: managed.NewMangedStorage(active),
 		identityKey:   identityKey,
 	}
 }
@@ -82,9 +44,9 @@ func NewWalletStorageManager(identityKey string, active wdk.WalletStorageProvide
 // MakeAvailable makes the storage available for the user.
 func (m *WalletStorageManager) MakeAvailable(ctx context.Context) (*wdk.TableSettings, error) {
 	if m.isAvailable {
-		return m.activeStorage.settings, nil
+		return m.activeStorage.Settings, nil
 	}
-	settings, err := m.activeStorage.makeAvailable(ctx, m.identityKey)
+	settings, err := m.activeStorage.MakeAvailableStorage(ctx, m.identityKey)
 	if err != nil {
 		return nil, fmt.Errorf("failed to make available active storage: %w", err)
 	}
@@ -105,9 +67,9 @@ func (m *WalletStorageManager) GetAuth(ctx context.Context) (wdk.AuthID, error) 
 	// TODO: handle that the active storage is not really an active storage
 
 	return wdk.AuthID{
-		UserID:      to.Ptr(m.activeStorage.user.UserID),
+		UserID:      to.Ptr(m.activeStorage.User.UserID),
 		IdentityKey: m.identityKey,
-		IsActive:    to.Ptr(m.activeStorage.settings.StorageIdentityKey == m.activeStorage.user.ActiveStorage),
+		IsActive:    to.Ptr(m.activeStorage.Settings.StorageIdentityKey == m.activeStorage.User.ActiveStorage),
 	}, nil
 }
 
