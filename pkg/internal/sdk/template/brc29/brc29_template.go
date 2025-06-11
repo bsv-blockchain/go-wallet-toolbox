1c6a980352d926480350ae2a81e2e0096278f13f
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-06-11 12:20:10 +0200

    feat(#274): create BRC29 script template (#275)

---
+++ pkg/internal/sdk/template/brc29/brc29_template.go
@@ -0,0 +1,101 @@
+package brc29
+
+import (
+	"fmt"
+
+	"github.com/4chain-ag/go-wallet-toolbox/pkg/internal/sdk"
+	"github.com/bsv-blockchain/go-sdk/script"
+	"github.com/bsv-blockchain/go-sdk/transaction"
+	"github.com/bsv-blockchain/go-sdk/transaction/template/p2pkh"
+)
+
+// Lock generates a locking script for a BRC29 address derived from the sender, key ID, and recipient public key.
+//
+// Arguments:
+//   - sender: the sender key. Can be a private key hex string or a key deriver or ec.PrivateKey.
+//   - keyID: the key ID.
+//   - recipient: the recipient key. This is the public key from private key that will be able to unlock it later. Can be a public key hex or a key deriver or ec.PublicKey.
+//   - opts: additional options.
+func Lock[SenderKey CounterpartyPrivateKey, RecipientKey CounterpartyPublicKey](senderPrivateKeySource SenderKey, keyID KeyID, recipientPublicKeySource RecipientKey, opts ...func(*lockOptions)) (*script.Script, error) {
+	address, err := Address(senderPrivateKeySource, keyID, recipientPublicKeySource, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate BRC29 address to lock the output: %w", err)
+	}
+
+	lockingScript, err := p2pkh.Lock(address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to lock the output with BRC29: %w", err)
+	}
+	return lockingScript, nil
+}
+
+var _ transaction.UnlockingScriptTemplate = (*UnlockingScriptTemplate)(nil)
+
+// UnlockingScriptTemplate is transaction.UnlockingScriptTemplate implementation for BRC29.
+type UnlockingScriptTemplate struct {
+	unlocker *p2pkh.P2PKH
+}
+
+// Unlock generates an unlocking script for a BRC29 address derived from the sender, key ID, and recipient private key.
+//
+// Arguments:
+//   - senderPublicKeySource: the sender key. Can be a private key hex string or a key deriver or ec.PrivateKey.
+//   - keyID: the key ID.
+//   - recipientPrivateKeySource: the recipient key. This is the private key for which the output was locked for. Can be a private key hex string or a key deriver or ec.PrivateKey.
+//   - opts: additional options.
+//
+// Additional options:
+//   - WithSigHash: the sighash type to use for signing.
+func Unlock[SenderKey CounterpartyPublicKey, RecipientKey CounterpartyPrivateKey](senderPublicKeySource SenderKey, keyID KeyID, recipientPrivateKeySource RecipientKey, opts ...func(*unlockOptions)) (*UnlockingScriptTemplate, error) {
+	options := &unlockOptions{}
+
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	senderIdentityKey, err := toIdentityKey(senderPublicKeySource)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create sender identity key from %T: %w", senderIdentityKey, err)
+	}
+
+	recipientKeyDeriver, err := toKeyDeriver(recipientPrivateKeySource)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create recipient key deriver from %T: %w", recipientPrivateKeySource, err)
+	}
+
+	err = keyID.Validate()
+	if err != nil {
+		return nil, fmt.Errorf("invalid key ID: %w", err)
+	}
+
+	key, err := recipientKeyDeriver.DerivePrivateKey(Protocol, keyID.String(), sdk.Counterparty{
+		Type:         sdk.CounterpartyTypeOther,
+		Counterparty: senderIdentityKey,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to derive BRC29 private key for unlocking: %w", err)
+	}
+
+	unlocker, err := p2pkh.Unlock(key, options.sigHash)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create BRC29 unlocker: %w", err)
+	}
+
+	return &UnlockingScriptTemplate{
+		unlocker: unlocker,
+	}, nil
+}
+
+// Sign signs the transaction input with BRC29.
+func (u *UnlockingScriptTemplate) Sign(tx *transaction.Transaction, inputIndex uint32) (*script.Script, error) {
+	unlockingScript, err := u.unlocker.Sign(tx, inputIndex)
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign input %d with BRC29: %w", inputIndex, err)
+	}
+	return unlockingScript, nil
+}
+
+// EstimateLength estimates the length of the BRC29 unlocking script for the input.
+func (u *UnlockingScriptTemplate) EstimateLength(tx *transaction.Transaction, inputIndex uint32) uint32 {
+	return u.unlocker.EstimateLength(tx, inputIndex)
+}
---
+++ pkg/internal/sdk/template/brc29/brc29_template_test.go
@@ -0,0 +1,216 @@
+package brc29_test
+
+import (
+	"testing"
+
+	"github.com/4chain-ag/go-wallet-toolbox/pkg/internal/sdk"
+	"github.com/4chain-ag/go-wallet-toolbox/pkg/internal/sdk/template/brc29"
+	"github.com/bsv-blockchain/go-sdk/chainhash"
+	ec "github.com/bsv-blockchain/go-sdk/primitives/ec"
+	"github.com/bsv-blockchain/go-sdk/script/interpreter"
+	"github.com/bsv-blockchain/go-sdk/transaction"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBRC29TemplateLock(t *testing.T) {
+	t.Run("should lock with P2PKH and BRC29 calculated address", func(t *testing.T) {
+		// when:
+		lockingScript, err := brc29.Lock(senderPrivateKeyHex, keyID, recipientPublicKeyHex)
+		// then:
+		assert.NoError(t, err)
+		require.NotNil(t, lockingScript)
+
+		// and:
+		address, err := lockingScript.Address()
+		assert.NoError(t, err, "Must get address from BRC29 locking script")
+		require.NotNil(t, address, "Must get address from BRC29 locking script")
+
+		assert.Equal(t, expectedAddress, address.AddressString)
+	})
+
+	t.Run("return error when nil is passed as sender private key deriver", func(t *testing.T) {
+		// given:
+		var keyDeriver *sdk.KeyDeriver
+
+		// when:
+		lockingScript, err := brc29.Lock(keyDeriver, keyID, recipientPublicKeyHex)
+
+		// then:
+		assert.Error(t, err)
+		require.Nil(t, lockingScript)
+	})
+
+	t.Run("return error when nil is passed as sender private key", func(t *testing.T) {
+		// given:
+		var priv *ec.PrivateKey
+
+		// when:
+		lockingScript, err := brc29.Lock(priv, keyID, recipientPublicKeyHex)
+
+		// then:
+		assert.Error(t, err)
+		require.Nil(t, lockingScript)
+	})
+
+	t.Run("return error when nil is passed as recipient public key deriver", func(t *testing.T) {
+		// given:
+		var keyDeriver *sdk.KeyDeriver
+
+		// when:
+		lockingScript, err := brc29.Lock(senderPrivateKeyHex, keyID, keyDeriver)
+
+		// then:
+		assert.Error(t, err)
+		require.Nil(t, lockingScript)
+	})
+
+	t.Run("return error when nil is passed as recipient public key", func(t *testing.T) {
+		// given:
+		var pub *ec.PublicKey
+
+		// when:
+		lockingScript, err := brc29.Lock(senderPrivateKeyHex, keyID, pub)
+
+		// then:
+		assert.Error(t, err)
+		require.Nil(t, lockingScript)
+	})
+
+	errorTestCases := map[string]struct {
+		sender    string
+		keyID     brc29.KeyID
+		recipient string
+	}{
+		"return error when sender key is empty": {
+			sender:    "",
+			keyID:     keyID,
+			recipient: invalidKeyHex,
+		},
+		"return error when sender key parsing fails": {
+			sender:    invalidKeyHex,
+			keyID:     keyID,
+			recipient: recipientPublicKeyHex,
+		},
+		"return error when KeyID is invalid": {
+			sender:    senderPrivateKeyHex,
+			keyID:     brc29.KeyID{DerivationPrefix: "", DerivationSuffix: ""},
+			recipient: recipientPublicKeyHex,
+		},
+		"return error when recipient key is empty": {
+			sender:    senderPrivateKeyHex,
+			keyID:     keyID,
+			recipient: "",
+		},
+		"return error when recipient key parsing fails": {
+			sender:    senderPrivateKeyHex,
+			keyID:     keyID,
+			recipient: invalidKeyHex,
+		},
+	}
+	for name, test := range errorTestCases {
+		t.Run(name, func(t *testing.T) {
+			// when:
+			lockingScript, err := brc29.Lock(test.sender, test.keyID, test.recipient)
+
+			// then:
+			require.Nil(t, lockingScript)
+			require.Error(t, err)
+		})
+	}
+}
+
+func TestBRC29TemplateUnlock(t *testing.T) {
+	t.Run("unlock the output locked with BRC29 locker", func(t *testing.T) {
+		// given:
+		prevTxID, err := chainhash.NewHashFromHex("64faeaa2e3cbadaf82d8fa8c7ded508cb043c5d101671f43c084be2ac6163148")
+		require.NoError(t, err)
+
+		// and:
+		tx := transaction.NewTransaction()
+		err = tx.AddOpReturnOutput([]byte("anything"))
+		require.NoError(t, err)
+
+		// and:
+		lockingScript, err := brc29.Lock(senderPrivateKeyHex, keyID, recipientPublicKeyHex)
+		require.NoError(t, err)
+
+		// when:
+		unlocker, err := brc29.Unlock(senderPublicKeyHex, keyID, recipientPrivateKeyHex)
+		require.NoError(t, err)
+
+		// and:
+		utxo := &transaction.UTXO{
+			TxID:                    prevTxID,
+			Vout:                    0,
+			Satoshis:                1000,
+			LockingScript:           lockingScript,
+			UnlockingScriptTemplate: unlocker,
+		}
+
+		err = tx.AddInputsFromUTXOs(utxo)
+		require.NoError(t, err)
+
+		// and:
+		err = tx.Sign()
+		require.NoError(t, err)
+
+		// then:
+		err = interpreter.NewEngine().Execute(interpreter.WithTx(tx, 0, tx.Inputs[0].SourceTxOutput()),
+			interpreter.WithAfterGenesis(),
+			interpreter.WithForkID())
+
+		require.NoError(t, err)
+	})
+
+	t.Run("estimate unlocking script length", func(t *testing.T) {
+		unlocker, err := brc29.Unlock(senderPublicKeyHex, keyID, recipientPrivateKeyHex)
+		require.NoError(t, err)
+
+		length := unlocker.EstimateLength(transaction.NewTransaction(), 0)
+
+		assert.EqualValues(t, 106, length)
+	})
+
+	errorTestCases := map[string]struct {
+		sender    string
+		keyID     brc29.KeyID
+		recipient string
+	}{
+		"return error when sender key is empty": {
+			sender:    "",
+			keyID:     keyID,
+			recipient: recipientPrivateKeyHex,
+		},
+		"return error when sender key parsing fails": {
+			sender:    invalidKeyHex,
+			keyID:     keyID,
+			recipient: recipientPrivateKeyHex,
+		},
+		"return error when KeyID is invalid": {
+			sender:    senderPublicKeyHex,
+			keyID:     brc29.KeyID{DerivationPrefix: "", DerivationSuffix: ""},
+			recipient: recipientPrivateKeyHex,
+		},
+		"return error when recipient key is empty": {
+			sender:    senderPublicKeyHex,
+			keyID:     keyID,
+			recipient: "",
+		},
+		"return error when recipient key parsing fails": {
+			sender:    senderPublicKeyHex,
+			keyID:     keyID,
+			recipient: invalidKeyHex,
+		},
+	}
+	for name, test := range errorTestCases {
+		t.Run(name, func(t *testing.T) {
+			// when:
+			unlocker, err := brc29.Unlock(test.sender, test.keyID, test.recipient)
+
+			// then:
+			require.Nil(t, unlocker)
+			require.Error(t, err)
+		})
+	}
+}
