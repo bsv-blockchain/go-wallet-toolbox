261802c3b41154ba5b4f09480d9be3209235f122
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-06-17 11:53:09 +0200

    suggestion: how the example for internalize could look like

---
+++ examples/setup/setup.go
@@ -0,0 +1,38 @@
+package setup
+
+import (
+	"github.com/4chain-ag/go-wallet-toolbox/pkg/defs"
+	ec "github.com/bsv-blockchain/go-sdk/primitives/ec"
+	"github.com/bsv-blockchain/go-sdk/wallet"
+)
+
+type Setup struct {
+	environment Environment
+	identityKey ec.PublicKey
+	privateKey  ec.PrivateKey
+}
+
+func (s *Setup) CreateWallet() (*wallet.Wallet, func()) {
+	// 1. Create a new wallet from an setup.environment
+	// - I would like to have this also as an example for us how to improve wallet.New so that it would accept some config struct.
+	return nil, func() {
+		// cleanup function
+	}
+}
+
+type Environment struct {
+	BSVNetwork defs.BSVNetwork `mapstructure:"bsv_network"`
+}
+
+func CreateAlice() *Setup {
+	// 1. Load the Environment config with use of viper,
+	// 		a. see how we initialize infra.Config struct
+	//      b. make sure to validate it also here
+	//      c. I see 2 possible options to structurize config files
+	//          1. dedicated config file for each "user" - this seems to be more flexible - because we could allow adopters to specify the file from which to load the config
+	//          2. one config file with dedicated sections for each "user" - this seems to be more convenient and consistent because everything is in one place and for example network would be consistent between all users
+	// 		d. It would be good to also handle --network flag passed to the command (main()), viper have option to also handle the flags.
+	// 2. Initialize Setup fields based on network and config/environment
+	// 3. In case of errors, panic
+	return nil
+}
--- examples/src/method/internalize_tx.go
+++ examples/src/method/internalize_tx.go
@@ -15,3 +15,19 @@ func InternalizeTxHandler(ctx context.Context, internalizeArgs sdk.InternalizeAc
 
 	return *iar, nil
 }
+
+func InternalizeFromFaucet(txID string, wallet *sdk.Wallet) error {
+	if txID == "" {
+		panic("")
+	}
+
+	// 1 - get the beef from whats on chain /beef endpoint - see wocAPIGetBeefForTX and implementation here: https://github.com/4chain-ag/wallet-toolbox-examples/blob/55d8e75ce7cbf098b8b2b1e4a79fa7d47ba3d0fc/src/act/faucetInternalize.ts#L19
+	// - if internal server error is returned then we need probably to wait until tx is mined
+	//   * maybe we could send the feedback to Deggen that there is such an issue with the /beef endpoint
+	// - later we can use storage method for getting beef
+	// 2. When we have beef, prepare Internalize Action Args based on values from DerivationParts
+	// 3. Print arguments
+	// 4. Call wallet.InternalizeAction
+	// 5. Print result
+
+}
