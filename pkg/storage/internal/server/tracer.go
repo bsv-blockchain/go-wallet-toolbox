a5612378ac9161b82fda06a16f80fb47d5ea2654
Damian Orzepowski <damian.orzepowski@4chain.studio>
2025-07-09 14:55:56 +0200

    test: add tests for handler logging

--- pkg/storage/internal/server/tracer.go
+++ pkg/storage/internal/server/tracer.go
@@ -25,9 +25,11 @@ func tracer(logger *slog.Logger) jsonrpc.Tracer {
 
 		if logging.IsDebug(logger) {
 			args = append(args, slog.String("handler", params[0].Type().String()))
+
 			for i, param := range params[1:] {
 				args = append(args, slog.Any(fmt.Sprintf("param_%d", i), reflectValueToLoggable(param)))
 			}
+
 			for i, result := range results {
 				args = append(args, slog.Any(fmt.Sprintf("result_%d", i), reflectValueToLoggable(result)))
 			}
