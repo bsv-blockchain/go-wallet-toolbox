5df32cbdb5b29a4f7de121bb77e90d46adf0c627
jakubmkowalski <jakub.kowalski@4chain.studio>
2025-07-25 17:39:15 +0200

    feat: add default abort action reference for testing

--- pkg/internal/fixtures/default_abort_action_args.go
+++ pkg/internal/fixtures/default_abort_action_args.go
@@ -4,6 +4,11 @@ import (
 	sdk "github.com/bsv-blockchain/go-sdk/wallet"
 )
 
+const (
+	// DefaultAbortActionReference is a test reference for abort action
+	DefaultAbortActionReference = "s7Tcy8M+5fLQ/XAk"
+)
+
 // DefaultWalletAbortActionArgs returns default SDK AbortActionArgs for testing
 func DefaultWalletAbortActionArgs() sdk.AbortActionArgs {
 	return sdk.AbortActionArgs{
