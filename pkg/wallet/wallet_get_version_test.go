dc899897af3ec45714c617489649915efa68ec44
Jakub Kowalski <155538368+jakubmkowalski@users.noreply.github.com>
2025-08-12 09:52:25 +0200

    feat: add hapy path test

--- pkg/wallet/wallet_get_version_test.go
+++ pkg/wallet/wallet_get_version_test.go
@@ -4,14 +4,14 @@ import (
 	"strings"
 	"testing"
 
-	sdk "github.com/bsv-blockchain/go-sdk/wallet"
+	"github.com/bsv-blockchain/go-wallet-toolbox/pkg/defs"
 	"github.com/bsv-blockchain/go-wallet-toolbox/pkg/wallet/internal/testabilities"
+	"github.com/stretchr/testify/require"
 )
 
 func TestWalletGetVersionArgsValidation(t *testing.T) {
 	errorTestCases := map[string]struct {
 		originator string
-		args       sdk.RelinquishOutputArgs
 	}{
 		"too long originator": {
 			originator: strings.Repeat("a", 251),
@@ -34,11 +34,26 @@ func TestWalletGetVersionArgsValidation(t *testing.T) {
 			aliceWallet := given.AliceWalletWithStorage(testabilities.StorageTypeMocked)
 
 			// when:
-			result, err := aliceWallet.GetVersion(t.Context(), test.args, test.originator)
+			result, err := aliceWallet.GetVersion(t.Context(), nil, test.originator)
 
 			// then:
 			then.Result(result).HasError(err)
 			then.Storage().HadNoInteraction()
 		})
 	}
+}
+
+func TestWalletGetVersion(t *testing.T) {
+	// given:
+	given, then, cleanup := testabilities.New(t)
+	defer cleanup()
+	aliceWallet := given.AliceWalletWithStorage(testabilities.StorageTypeMocked)
+
+	// when:
+	result, err := aliceWallet.GetVersion(t.Context(), nil, "alice.wallet")
+
+	// then:
+	require.NoError(t, err)
+	require.Equal(t, defs.Version, result.Version)
+	then.Storage().HadNoInteraction()
 }
