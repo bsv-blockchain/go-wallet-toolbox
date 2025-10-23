2fb35e0ffc1a5d0412d3c9826a8eba7b425ab36f
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2025-10-23 10:54:32 +0200

    feat: implement Chaintracks service with lifecycle management

---
+++ pkg/services/chaintracks/chaintracks_service_test.go
@@ -0,0 +1,29 @@
+package chaintracks_test
+
+import (
+	"testing"
+
+	"github.com/bsv-blockchain/go-wallet-toolbox/pkg/defs"
+	"github.com/bsv-blockchain/go-wallet-toolbox/pkg/internal/logging"
+	"github.com/bsv-blockchain/go-wallet-toolbox/pkg/services/chaintracks"
+	"github.com/stretchr/testify/require"
+)
+
+func TestChaintracksService_Livecycle(t *testing.T) {
+	// given:
+	service, err := chaintracks.NewService(logging.NewTestLogger(t), defs.DefaultChaintracksServiceConfig())
+	require.NoError(t, err)
+
+	// when:
+	err = service.MakeAvailable(t.Context())
+
+	// then:
+	require.NoError(t, err, "make available should not return error")
+	require.True(t, service.Available(), "service should be available after make available")
+
+	// when:
+	service.Destroy()
+
+	// then:
+	require.False(t, service.Available(), "service should be unavailable after destroy")
+}
