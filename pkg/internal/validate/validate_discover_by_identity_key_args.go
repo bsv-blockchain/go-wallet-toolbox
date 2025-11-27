a45d60bb2103668400ebac51e65c65eb5d1147e5
Damian <dzoltow@gmail.com>
2025-11-27 23:52:45 +0100

    feat: validatie discoverByIdentityKeyArgs

---
+++ pkg/internal/validate/validate_discover_by_identity_key_args.go
@@ -0,0 +1,35 @@
+package validate
+
+import (
+	"fmt"
+
+	sdk "github.com/bsv-blockchain/go-sdk/wallet"
+	"github.com/bsv-blockchain/go-wallet-toolbox/pkg/wdk/primitives"
+)
+
+// DiscoverByIdentityKeyArgs validates arguments for DiscoverByIdentityKey()
+func DiscoverByIdentityKeyArgs(args sdk.DiscoverByIdentityKeyArgs) error {
+	if args.Limit != nil {
+		if *args.Limit < MinPaginationLimit {
+			return fmt.Errorf("limit must be greater than 0")
+		}
+		if *args.Limit > MaxPaginationLimit {
+			return fmt.Errorf("limit exceeds max allowed value of %d", MaxPaginationLimit)
+		}
+	}
+
+	if args.Offset != nil {
+		if *args.Offset > MaxPaginationOffset {
+			return fmt.Errorf("offset is too large")
+		}
+	}
+
+	if args.IdentityKey != nil {
+		hex := primitives.PubKeyHex(args.IdentityKey.ToDERHex())
+		if err := hex.Validate(); err != nil {
+			return fmt.Errorf("invalid verifier: failed validation check")
+		}
+	}
+
+	return nil
+}
