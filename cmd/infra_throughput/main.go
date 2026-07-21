781411a5124676f3d121a0d8a2266613c9bf1d53
Deggen <d.kellenschwiler@bsvassociation.org>
2026-07-20 23:17:00 -0500

    feat(infra): add throughput server cmd and docker config

---
+++ cmd/infra_throughput/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bsv-blockchain/go-wallet-toolbox/pkg/infra"
+)
+
+func main() {
+	server, err := infra.NewServer(
+		context.Background(),
+		infra.WithConfigFile("infra-config-throughput.yaml"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		if err = server.ListenAndServe(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	server.Cleanup()
+}
